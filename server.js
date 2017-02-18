7b37effe743b4112c900a259c53c63ac3748c901
Mohd Nadeem <mohd.nadeem3464@gmail.com>
2017-02-18 20:04:38 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+//express.static() -> built in middleware
+//path to serving directory -> __dirname+'/public'
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err)=>{
+    if(err){
+      console.log("Some error Occured");
+    }
+  });
+  next();
+});
+
+app.use((req,res,next)=>{
+  res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getYear', () => {
+  //return 'text';
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+// app.get('/',(req, res)=>{
+//   res.send("This is working");
+// });
+
+app.get('/',(req, res)=>{
+  res.render('home.hbs',{
+    welcomeMsg : "Hello! How are you?",
+    pageTitle : "Home Page",
+    companyName : "ABC"
+    //curYear : new Date().getFullYear()
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle : "About Page",
+    companyName : "ABC"
+    //curYear : new Date().getFullYear()
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMsg : "Some Error Occured"
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log("Server is up at port 3000");
+});
